47fae4d6fdd1f2565014a0462a58e1a41138f02e
oenriquez <enriquez.oscar@outlook.com>
2015-10-03 14:48:37 -0500

    segunda aproximacion

--- main.cc
+++ main.cc
@@ -2,17 +2,35 @@
 #include <iostream>
 #include <stdio.h>
 
-class Listadeindentidad{
+class Operetion{
+	public: 
+		virtual int operate( int a, int b) = 0;
+		
+};
+class Addition : public Operetion{
 	public:
-		virtual bool altaPrioridad();
-		virtual void elSiguienteprioridad(); 
-		virtual int  prioridadgradada();
+	int operate(int a, int b){
+		return a + b;
+	}	
 };
 
 
+class Difference : public Operetion{
+	public:
+	int operate(int a, int b){
+		return a - b;
+	}	
+};
 
+void printfOperation(int a, int b, Operetion& op){
+	std::cout<< a <<" ? "<< b << " = "<< op.operate(a,b) << std::endl;
+
+}
 
 int main(){
-
+	Addition add;
+	Difference diff;
+	printfOperation(2,3,add);
+	printfOperation(2,3,diff);
 	return 0;
 };
